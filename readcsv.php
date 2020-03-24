f723017a3f3780744c3a8d8dafa46757322a6233
sumeta <sumeta.pon@gmail.com>
2020-03-24 21:58:06 +0700

    add ref

--- readcsv.php
+++ readcsv.php
@@ -1,5 +1,7 @@
 <?php
 
+    // ref : https://www.php.net/manual/en/function.fgetcsv.php
+
     $file = fopen("product.csv","r");
     while (($row = fgetcsv($file, 1000, ",")) !== FALSE) {
         echo $row[0] . ' ' . $row[1];
